84b8b92825f3de154905326f23d3cc4502504fcf
unknown <malakbelkho1721@gmail.com>
2026-04-08 20:36:58 +0100

    HelloToast

---
+++ app/src/main/java/ma/ens/hellotoast/MainActivity.java
@@ -0,0 +1,39 @@
+package ma.ens.hellotoast;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCurrentValue = 0;
+    private TextView mScoreView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreView = findViewById(R.id.display_value);
+        Button triggerToast = findViewById(R.id.btn_toast);
+        Button triggerIncrement = findViewById(R.id.btn_count);
+
+        triggerToast.setOnClickListener(view -> {
+            String welcomeMsg = "Toast";
+            Toast.makeText(this, welcomeMsg, Toast.LENGTH_SHORT).show();
+        });
+
+        triggerIncrement.setOnClickListener(view -> {
+            incrementScore();
+        });
+    }
+
+    private void incrementScore() {
+        mCurrentValue++;
+        if (mScoreView != null) {
+            mScoreView.setText(String.format("%d", mCurrentValue));
+        }
+    }
+}
